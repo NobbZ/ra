c5214ad18513e277abef45b3a899452431bdd115
Norbert Melzer <timmelzer@gmail.com>
2017-07-17 00:02:54 +0200

    Bump version 0.3.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ra.Mixfile do
 
   def project do
     [app: :ra,
-     version: "0.3.2",
+     version: "0.3.3",
      elixir: "~> 1.0",
      package: package(),
      docs: [readme: true, main: "README.md"],
